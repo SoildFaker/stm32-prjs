7c592447a15cb89339844e5c6a2d8f123793649c
enbinli <enbinli@outlook.com>
2016-07-07 20:01:31 +0800

    update

--- incpath.sh
+++ incpath.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cur="/home/faker/Documents/code/STM/stm32-quadrocopter"
-export C_INCLUDE_PATH="${cur}/lib/inc:${cur}/drivers/mpu:${cur}/drivers:${cur}/cmsis:${cur}/project"
+export C_INCLUDE_PATH="${cur}/lib/inc:${cur}/drivers:${cur}/cmsis:${cur}/project"
 
