67bb1398b8085ff8c8f00839879f52a2d3de5f43
enbinli <enbinli@outlook.com>
2016-07-10 17:06:31 +0800

    branch pid

--- drivers/pid.c
+++ drivers/pid.c
@@ -9,7 +9,6 @@ uint8_t control_mode = 0;
 float loop_time = 0.0f;
 float height = 0.0f;
 float rx_value[6] = {0.00f,0.00f,0.28f,0.0f,0.0f,0.0f};//store the pulse times of 6 input pwm chanels from rx device
-
 									// rx_value[0]: target_x_angle/target_x_rate
 									// rx_value[1]: target_y_angle/target_y_rate
 									// rx_value[2]: throttle
