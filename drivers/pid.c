d87a8f20f46a56a2b04b41b80505860a8fd0776d
enbinli <enbinli@outlook.com>
2016-08-20 22:06:22 +0800

    ok

--- drivers/pid.c
+++ drivers/pid.c
@@ -7,7 +7,7 @@
 
 float rx_value[6] = {
   0.0f,0.0f,0.0f,//roll, pitch, yaw
-  0.0f,0.0f,20.0f //x,y,z point
+  0.0f,0.0f,30.0f //x,y,z point
 };
 float throttle = 0.50f;
 float ERRz,_ERRz,ERRzD,PIDz_Out;
@@ -30,7 +30,7 @@ float PID_Value[21]={
   9.640, 0.003, 5.081,//yoll PID
   7.085, 0.002, 0.016,//y coordinate PID
   9.643, 0.003, 5.081,//pitch PID
-  0.523, 5.022, 0.016,//z coordinate PID
+  0.223, 0.922, 0.016,//z coordinate PID
   8.060, 0.000, 0.007,//yaw PID
   35.00, 35.00, 10.00 //
 };
@@ -150,10 +150,10 @@ void MORTOR_Output(void)
   }
   else
   {   
-    pwm[0]=(uint16_t)(throttle*PWM_RANGE)+MIN_PWM+PIDx+PIDy+PIDz-(uint16_t)PID_Value[21];
-    pwm[1]=(uint16_t)(throttle*PWM_RANGE)+MIN_PWM-PIDx+PIDy-PIDz-(uint16_t)PID_Value[21];
-    pwm[2]=(uint16_t)(throttle*PWM_RANGE)+MIN_PWM-PIDx-PIDy+PIDz+(uint16_t)PID_Value[21]; 
-    pwm[3]=(uint16_t)(throttle*PWM_RANGE)+MIN_PWM+PIDx-PIDy-PIDz+(uint16_t)PID_Value[21];
+    pwm[0]=(uint16_t)(throttle*PWM_RANGE)+MIN_PWM+PIDx+PIDy-PIDz-(uint16_t)PID_Value[21];
+    pwm[1]=(uint16_t)(throttle*PWM_RANGE)+MIN_PWM-PIDx+PIDy+PIDz-(uint16_t)PID_Value[21];
+    pwm[2]=(uint16_t)(throttle*PWM_RANGE)+MIN_PWM-PIDx-PIDy-PIDz+(uint16_t)PID_Value[21]; 
+    pwm[3]=(uint16_t)(throttle*PWM_RANGE)+MIN_PWM+PIDx-PIDy+PIDz+(uint16_t)PID_Value[21];
 
     for(i=0;i<4;i++)
     {
--- project/main.c
+++ project/main.c
@@ -4,13 +4,14 @@
 #include "tools.h"
 #include <stdio.h>
 #include "ahrs.h"
+/*#include "magnet.h"*/
 #include "mpu6050.h"
 #include "flow.h"
 #include "nvic.h"
 #include "conf.h"
 #include "pid.h"
 /* Exported constants --------------------------------------------------------*/
-uint8_t State = 0;
+uint8_t stop = 0;
 int main(void)
 {
   SystemInit();
@@ -18,8 +19,8 @@ int main(void)
 
   MPU6050_Init();
   ADNS3080_Init();
+  /*MAG3110_Init();*/
 
-  uint8_t stop = 0;
   uint8_t dt = 0;
   uint16_t idt = 0;
   uint8_t a = 0;
@@ -38,24 +39,24 @@ int main(void)
       MORTOR_Output();
       a++;
       idt+=dt;
-      if (a == 0){
+      if (a%1000 == 0){
         height = HCSR04_Get();
         
         PIDzp_Update((float)1e-6*idt);
-        myprintf("============================================================\r\n");
+        /*myprintf("============================================================\r\n");*/
         /*myprintf("height:%f\tthrottle:%f\tdt:%d\tidt:%d\r\n", HCSR04_Get(), throttle, dt, idt);*/
-        myprintf("C1:%d\tC2:%d\tC3:%d\tC4:%d\r\n", TIM4->CCR1, TIM4->CCR2, TIM4->CCR3, TIM4->CCR4);
-        myprintf("roll:%f\tpitch:%f\tyaw:%f\r\n", roll, pitch, yaw);
+        /*myprintf("C1:%d\tC2:%d\tC3:%d\tC4:%d\r\n", TIM4->CCR1, TIM4->CCR2, TIM4->CCR3, TIM4->CCR4);*/
+        /*myprintf("roll:%f\tpitch:%f\tyaw:%f\r\n", roll, pitch, yaw);*/
         /*myprintf("X:%d\tY:%d\tZ:%f\r\n", X, Y, height);*/
         /*myprintf("TIM3:%d\r\n",tim3_count);*/
         idt=0;
         b++;
       }
-      if (b>14){
-        stop = 0;
+      if (b>8){
+        stop = 1;
       }
     }else{
-      throttle = 0.0f; 
+      throttle -= 0.00005f; 
       MORTOR_Output();
     }
   }
